64d20fee78f82abf942477ec25d21ac004dc5aa3
Pradeep Ravindran <11547169+agnnn@users.noreply.github.com>
2025-12-16 17:42:18 +0530

    fix: remove echo statement from Terraform validate command and format locals.tf for consistency

--- oracle-ebs-framework/locals.tf
+++ oracle-ebs-framework/locals.tf
@@ -1,7 +1,7 @@
 locals {
-  ebs_apps_label         = "oracle-ebs-apps"
-  ebs_db_label           = "oracle-ebs-db"
-  vision_label           = "oracle-ebs-vision"
+  ebs_apps_label = "oracle-ebs-apps"
+  ebs_db_label   = "oracle-ebs-db"
+  vision_label   = "oracle-ebs-vision"
 
   common_tags = toset([
     "lb-health-check",
@@ -12,22 +12,22 @@ locals {
   ])
 
   vm_network_tags = {
-    app = toset(concat(local.common_tags, [
+    app = toset(concat(tolist(local.common_tags), [
       "http-server",
       "https-server",
       local.ebs_apps_label,
       "external-app-access",
     ]))
 
-    db = toset(concat(local.common_tags, [
+    db = toset(concat(tolist(local.common_tags), [
       local.ebs_db_label,
       "external-db-access"
     ]))
 
-    vision = toset(concat(local.common_tags, [
+    vision = toset(concat(tolist(local.common_tags), [
       "http-server",
       "https-server",
-      local.ebs_apps_label, # Vision instance includes an app server
+      local.ebs_apps_label,
       "external-app-access",
       "external-db-access"
     ]))
